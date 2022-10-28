15b252e0e7aa73cc8606b579edb2915c3c20f338
ttdo19 <thuytrang19072001@gmail.com>
2022-10-28 18:43:28 -0400

    My initial commit

---
+++ Carpany/User.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  Carpany
+//
+//  Created by Trang Do on 10/18/22.
+//
+
+import Foundation
+import ParseSwift
+
+struct User: ParseUser {
+    
+  // Additional properties required by the ParseUser protocol
+  var authData: [String : [String : String]?]?
+  var originalData: Data?
+  var objectId: String?
+  var createdAt: Date?
+  var updatedAt: Date?
+  var ACL: ParseACL?
+  
+  // Main properties linked to the user's information
+  var username: String?
+  var email: String?
+  var emailVerified: Bool?
+  var password: String?
+
+  // A custom property
+  var bio: String?
+}
